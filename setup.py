5ba66e64bff8a8aafef494613797337544878cdf
Kashaf Shaikh <kashafshaikh1284@gmail.com>
2026-04-22 12:05:02 +0530

    Add complete working Medical Chatbot implementation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical_chatbot",
+    name="medical-chatbot",
     version="0.1.0",
     author="Kashaf Shaikh",
     author_email="kashafshaikh1284@gmail.com",
